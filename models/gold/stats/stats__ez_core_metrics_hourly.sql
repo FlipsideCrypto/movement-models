370282ec89e00cffe5c159be4c9fec8bee38bbc6
Mike Stepanovic <mike.stepanovic@flipsidecrypto.com>
2025-04-07 15:00:30 -0600

    switched to VAR lookup

--- models/gold/stats/stats__ez_core_metrics_hourly.sql
+++ models/gold/stats/stats__ez_core_metrics_hourly.sql
@@ -8,6 +8,33 @@
     tags = ['noncore']
 ) }}
 
+-- depends_on: {{ ref('core__fact_transactions') }}
+
+{% if execute %}
+
+{% if is_incremental() %}
+{% set query %}
+
+SELECT
+    DATE_TRUNC('hour', MIN(block_timestamp)) AS block_timestamp_hour 
+        FROM
+            {{ ref('core__fact_transactions') }}
+        WHERE
+            modified_timestamp >= (
+                SELECT
+                    MAX(modified_timestamp)
+                FROM
+                    {{ this }}
+            )
+         {% endset %}
+    {% set min_block_timestamp_hour = run_query(query).columns [0].values() [0] %}
+{% endif %}
+
+{% if not min_block_timestamp_hour or min_block_timestamp_hour == 'None' %}
+    {% set min_block_timestamp_hour = '2099-01-01' %}
+{% endif %}
+{% endif %}
+
 SELECT
     block_timestamp_hour,
     block_number_min,
@@ -32,16 +59,16 @@ SELECT
 FROM
     {{ ref('silver_stats__core_metrics_hourly') }}
     s
-    JOIN {{ ref('price__ez_prices_hourly') }}
+    LEFT JOIN {{ ref('price__ez_prices_hourly') }}
     p
     ON s.block_timestamp_hour = p.hour
     AND p.is_native
 WHERE
     block_timestamp_hour < DATE_TRUNC('hour', CURRENT_TIMESTAMP)
 {% if is_incremental() %}
-    AND block_timestamp_hour >= DATEADD('hour', -{{ var('HOURLY_METRICS_LOOKBACK_HOURS', 24) }}, (
-        SELECT 
-            DATE_TRUNC('hour', MAX(modified_timestamp))
-        FROM {{ this }}
-    ))
+AND
+    block_timestamp_hour >= COALESCE(
+        DATEADD('hour', -4, '{{ min_block_timestamp_hour }}'),
+        '2025-01-01 00:00:00'
+    )
 {% endif %}
