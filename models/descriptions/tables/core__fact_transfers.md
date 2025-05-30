6b844e4c7117b40abd9482bc9501c8cd19cc6828
Mike Stepanovic <mike.stepanovic@flipsidecrypto.com>
2025-05-30 10:34:20 -0600

    tweak tests and docs

--- models/descriptions/tables/core__fact_transfers.md
+++ models/descriptions/tables/core__fact_transfers.md
@@ -1,4 +1,4 @@
-{% docs core__fact_transactions %}
+{% docs core__fact_transfers %}
 
 This table contains Deposit and Withdraw events on the Movement blockchain. Note: transfers with a 0 amount are excluded.
 
--- models/gold/core/core__fact_transfers.sql
+++ models/gold/core/core__fact_transfers.sql
@@ -1,7 +1,8 @@
 {{ config(
     materialized = 'incremental',
-    unique_key = ['tx_hash','event_index','block_timestamp::DATE'],
+    unique_key = ['tx_hash','event_index','version','block_timestamp::DATE'],
     incremental_strategy = 'merge',
+    incremental_predicates = ["dynamic_range_predicate", "block_timestamp::DATE"],
     merge_exclude_columns = ["inserted_timestamp"],
     cluster_by = ['block_timestamp::DATE','modified_timestamp::DATE'],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(tx_hash, version, account_address,token_address);",
@@ -20,9 +21,11 @@ SELECT
     account_address,
     amount,
     token_address,
-    transfers_id AS fact_transfers_id,
-    inserted_timestamp,
-    modified_timestamp
+    {{ dbt_utils.generate_surrogate_key(
+        ['tx_hash','event_index','version']
+    ) }} AS fact_transfers_id,
+    SYSDATE() AS inserted_timestamp,
+    SYSDATE() AS modified_timestamp
 FROM
     {{ ref(
         'silver__transfers'
